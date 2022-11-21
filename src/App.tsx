3c96a03924bbe4a78b872c51b06b00bbdf130de6
zero <zeropersonalitydisorder@gmail.com>
2022-11-21 22:02:42 +0100

    add Dockerfile

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    (async () => {
+      await axios.get("http://localhost:5000/api/v1/greeting");
+    })();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
