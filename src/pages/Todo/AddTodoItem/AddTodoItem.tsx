d824ad52987e1eef3955f4105b8f27ab1b0eeaa6
zero <zeropersonalitydisorder@gmail.com>
2022-11-28 20:25:22 +0100

    handle enter keydown event

--- src/pages/Todo/AddTodoItem/AddTodoItem.tsx
+++ src/pages/Todo/AddTodoItem/AddTodoItem.tsx
@@ -1,5 +1,12 @@
-import { Button, Col, Input, Row } from "antd";
-import { ChangeEvent, FC, useState } from "react";
+import { Button, Col, Input, InputRef, Row } from "antd";
+import {
+  ChangeEvent,
+  FC,
+  useEffect,
+  useRef,
+  useState,
+  KeyboardEvent,
+} from "react";
 
 type AddTodoItemProps = {
   addNewItem: (title: string) => void;
@@ -7,6 +14,11 @@ type AddTodoItemProps = {
 
 const AddTodoItem: FC<AddTodoItemProps> = ({ addNewItem }) => {
   const [title, setTitle] = useState("");
+  const titleInput = useRef<InputRef>(null);
+
+  useEffect(() => {
+    titleInput.current?.focus();
+  }, []);
 
   const onTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setTitle(event.target.value);
@@ -17,13 +29,22 @@ const AddTodoItem: FC<AddTodoItemProps> = ({ addNewItem }) => {
     setTitle("");
   };
 
+  const onAddNewItemKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.code === "Enter") {
+      addNewItem(title);
+      setTitle("");
+    }
+  };
+
   return (
     <Row style={{ padding: 20 }} justify="space-between" gutter={[10, 10]}>
       <Col flex="auto">
         <Input
+          ref={titleInput}
           value={title}
           placeholder="What are we going to do"
           onChange={onTitleChange}
+          onKeyDown={onAddNewItemKeyDown}
         />
       </Col>
       <Col>
