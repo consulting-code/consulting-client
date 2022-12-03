a92d8d4d746dde43bb00026768512f97f6e684af
Kruchkov Yauhen <y.kruchkov@fh.by>
2022-12-03 20:45:46 +0300

    added initial redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 import App from "./App";
 
@@ -7,4 +8,8 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
